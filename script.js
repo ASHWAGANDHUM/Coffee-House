ce917b740ae2eabc27ada3e0c71a77a56e508fcf
ASHWAGANDHUM <agq772@gmail.com>
2025-07-24 23:24:48 +0800

    Add interctive map via API and style map container with rounded corners and shadow.

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 const menuButtonEl = document.querySelectorAll('.menu__button'); // NodeList с кнопками
 const menuCardsEl = document.querySelectorAll('.menu__cards'); // NodeList с блоками карточек
 
-menuButtonEl.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        menuButtonEl.forEach((b) => b.classList.remove('menu__button_active'));
-    btn.classList.add('menu__button_active');
-    });
-
-    btn.addEventListener('click', () => {
-        showCardMenu(index);
-    });
-
-});
-
 function showCardMenu(activeIndex) {
 
     if (menuCardsEl[activeIndex].classList.contains('menu__cards_show')) {
@@ -30,3 +18,43 @@ function showCardMenu(activeIndex) {
     }, 250);
 
 }
+
+menuButtonEl.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        menuButtonEl.forEach((b) => b.classList.remove('menu__button_active'));
+    btn.classList.add('menu__button_active');
+    });
+
+    btn.addEventListener('click', () => {
+        showCardMenu(index);
+    });
+
+});
+
+let place = [59.958021, 30.290751];
+
+function init() {
+    let map = new ymaps.Map('map', {
+        center: place,
+        zoom: 12
+    });
+
+    let placemark = new ymaps.Placemark(place, {}, {
+        iconLayout: 'default#image',
+        iconImageHref: './images/map/marker.svg',
+        iconImageSize: [150, 150],
+        iconImageOffset: [-18, -110]
+    });
+
+    map.controls.remove('geolocationControl'); // удаляем геолокацию
+    map.controls.remove('searchControl'); // удаляем поиск
+    map.controls.remove('trafficControl'); // удаляем контроль трафика
+    map.controls.remove('typeSelector'); // удаляем тип
+    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    map.controls.remove('zoomControl'); // даляем контроль зуммирования
+    map.controls.remove('rulerControl'); // удаляем контроль правил
+
+    map.geoObjects.add(placemark);
+}
+
+ymaps.ready(init);
