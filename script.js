f79c2f7bb6cf1b36eaf372142cb7afc0f7d9ee24
ASHWAGANDHUM <agq772@gmail.com>
2025-07-25 19:46:54 +0800

    Add and style footer block. Add interactive for images on top via JS code.

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-const menuButtonEl = document.querySelectorAll('.menu__button'); // NodeList с кнопками
-const menuCardsEl = document.querySelectorAll('.menu__cards'); // NodeList с блоками карточек
+const buttonToContacts = document.getElementById('contact-button');
+
+buttonToContacts.addEventListener('click', () => {
+    document.getElementById('contacts').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+const buttonToMenu = document.getElementById('menu-button');
+
+buttonToMenu.addEventListener('click', () => {
+    document.getElementById('menu').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
+const menuButtons = document.querySelectorAll('.menu__button'); // NodeList с кнопками
+const menuCards = document.querySelectorAll('.menu__cards'); // NodeList с блоками карточек
 
 function showCardMenu(activeIndex) {
 
-    if (menuCardsEl[activeIndex].classList.contains('menu__cards_show')) {
+    if (menuCards[activeIndex].classList.contains('menu__cards_show')) {
         return;
     };
 
-    menuCardsEl.forEach((card, index) => {
+    menuCards.forEach((card, index) => {
         if (index !== activeIndex) {
             card.classList.remove('menu__cards_show');
         };
     });
 
     setTimeout(() => {
-        menuCardsEl[activeIndex].classList.add('menu__cards_show');
+        menuCards[activeIndex].classList.add('menu__cards_show');
     }, 250);
 
 }
 
-menuButtonEl.forEach((btn, index) => {
+menuButtons.forEach((btn, index) => {
     btn.addEventListener('click', () => {
-        menuButtonEl.forEach((b) => b.classList.remove('menu__button_active'));
+        menuButtons.forEach((b) => b.classList.remove('menu__button_active'));
     btn.classList.add('menu__button_active');
     });
 
@@ -30,6 +47,35 @@ menuButtonEl.forEach((btn, index) => {
     });
 
 });
+
+//////////////// НАЖАТИЕ НА ИЗОБРАЖЕНИЕ В ВЕРХНЕЙ ЧАСТИ САЙТА
+
+const images = [
+    document.getElementById('coffee'),
+    document.getElementById('desserts'),
+    document.getElementById('snacks')
+];
+
+
+images.forEach((imgEl, index) => {
+  imgEl.addEventListener('click', () => {
+
+    document.getElementById('menu').scrollIntoView({
+        behavior: 'smooth'
+    });
+
+    // Убираем активный класс со всех кнопок
+    menuButtons.forEach(btn => btn.classList.remove('menu__button_active'));
+
+    // Немного ждем прокрутки и показываем нужный блок
+    setTimeout(() => {
+        menuButtons[index].classList.add('menu__button_active');
+      showCardMenu(index);
+    }, 500);
+  });
+});
+
+//////////////////// ЯНДЕКС.КАРТА
 
 let place = [59.958021, 30.290751];
 
